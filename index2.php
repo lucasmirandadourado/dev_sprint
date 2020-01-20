716e41534c4cc1e05f8ab6f58c9f917b5a289647
lucasmirandadourado <lucasmirandadourado@gmail.com>
2020-01-20 08:06:37 -0300

    criando tela de login

--- index2.php
+++ index2.php
@@ -2,36 +2,54 @@
 require_once('./template/header.php');
 ?>
 
-<link rel="stylesheet" href="asset/login.css">
+<style>
+     .container {
+        max-width: 420px;
+        margin: 0 auto;
+        display: flex;
+        border: 1px solid #ccc;
+     }
 
-<div class="container sprint">
-    <div id="sprints_cadastrados">
-        
-    </div>
-    <div id="chartContainer" style="height: 370px; max-width: 920px; margin: 0px auto;"></div>
+    .alinhamento-central {
+    	height: 400px;
+        justify-content: center;
+        align-items: center;
+    }
 
-    <table class="table table-striped table-bordered" id="sprint">
-        <thead>
-            <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>Qtd Colaborador</td>
-                <td>Data início</td>
-            </tr>
-        </thead>
-        <tbody id="tabela"></tbody>
-    </table>
+    .alinhamento-central .item {
+        flex: 0;        
+    }
+    .item {
+        text-align: left;
+        font-size: 1.5em;
+    }
+</style>
 
-</div>
-
+<section class="container alinhamento-central">
+    <form class="item">
+        <div  class="row">
+            <div class="form-group">
+                <label for="login">Login</label>
+                <input type="text" class="login">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group">
+                <label for="senha">Senha</label>
+                <input type="password">
+            </div>
+        </div>
+        <div class="row">
+            <button class="btn btn-success">Entrar</button>
+        </div>
+    </form>
+</section>
 
 
 <script src="./asset/chartjs/canvasjs.min.js"></script>
 <script src="./asset/js/jquery-3.3.1.min.js"></script>
 <script src="./asset/js/jquery.dataTables.min.js"></script>
 <script src="./asset/js/dataTables.bootstrap4.min.js"></script>
-<script src="./asset/js/buscarColaboradores.js"></script>
-
 
 <?php
 require_once('./template/footer.php');
