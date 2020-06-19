295c8a7cdb6f10fddf9b85a5f5c9f890c1f33790
Lucas Nunes <lucasmirandadourado@gmail.com>
2020-06-18 22:58:36 -0300

    Corrigindo o cadastro do sprint

--- service/SprintService.php
+++ service/SprintService.php
@@ -3,33 +3,43 @@
 require_once(dirname(__FILE__) . '/../model/Tarefa.php');
 require_once(dirname(__FILE__) . '/../factory/SprintFactory.php');
 require_once(dirname(__FILE__).'/../util/uteis.php');
+date_default_timezone_set('America/Sao_Paulo');
 
 class SprintService
 {
 
     public function __construct() {}
 
-    public static function cadastrarSprint($form)
+    public static function cadastrarSprint($formulario)
     {
-        $formulario = converterFormEmArray($form['cadastrarSprint']);
-        $validar = self::validacaoFormulario($formulario);
-        if(!$validar['status']) return $validar;
+        try {
+            $validar = self::validacaoFormulario($formulario);
+            if($validar['status'] == false) throw new Exception($validar['mensagem'], 1);
         
-        $nome = $formulario['nome'];
-        $qtdDev = $formulario['qtdDevs'];
-        $dias = $formulario['dias'];
-        $datas = $formulario['data[]'];
-        
-        $spt = new Sprint($nome, $datas[0], $datas[$dias - 1], $qtdDev);
-        
-        if (count($datas) > 0) {
-            foreach ($datas as $key => $value) {
-                $spt->addDia($value);
-            }
-            $spt->setDataFim(end($spt->getDatas()));
+            $nome = $formulario['nome'];
+            $qtdDev = $formulario['qtdDevs'];
+            $dias = $formulario['dias'];
+            $datas = $formulario['data'];
+
+            Util::validarCampo('string', $nome);
+            
+        } catch (\Throwable $th) {
+            return ['status' => false, 'mensagem' => $th->getMessage()];
         }
         
-        // FIXME: Bug quando for criar o objeto. Verificar as datas.
+        $spt = new Sprint($nome, $datas[0], $datas[$dias - 1], $qtdDev);
+        $datas_times = array();
+        if (count($datas) > 0) {
+            foreach ($datas as $key => $value) {
+                $data = DateTime::createFromFormat('Y-m-d', $value);
+                $spt->addDia($value);
+                array_push($datas_times, $data->getTimestamp());
+            }
+            rsort($datas_times);            
+            $spt->setDataInicio( date('Y-m-d', end($datas_times)));
+            $spt->setDataFim(date('Y-m-d', $datas_times[0]));
+        }
+        
         return SprintFactory::repository()->save($spt);
     }
 
@@ -56,7 +66,7 @@ class SprintService
     }
 
     public static function find($id) {
-        $validar = validarCampo("integer", $id);
+        $validar = Util::validarCampo("integer", $id);
         if($validar['status'] == false) return $validar;
         return SprintFactory::repository()->find($id);    
     }
--- util/uteis.php
+++ util/uteis.php
@@ -1,34 +1,37 @@
 <?php
-
-function converterFormEmArray($form) {
-    $formulario = array();
-    foreach ($form as $key => $value) {
-        if(strpos($value['name'], '[]')) {
-            $formulario[$value['name']][] = $value['value'];
-        } else $formulario[$value['name']] = trim($value['value']);
-    }
-    return $formulario;
-}
-
-function validarCampo($tipo, $valor, $mensagemErro = '') {
-
-    if($tipo=="integer") {        
-        if(!is_numeric($valor)) throw new Exception($mensagemErro == '' ? "Não é um valor válido." : $mensagemErro, 999);
-        if(empty($valor)) throw new Exception($mensagemErro == '' ? "Informe o código do Sprint." : $mensagemErro, 998);
-        if($valor <= 0) throw new Exception($mensagemErro == '' ? "Sprint inválido." : $mensagemErro, 998);
-        return ['status'=>true];       
-    } else if ($tipo == "string") {
-        if(empty(trim($valor))) throw new Exception($mensagemErro == '' ? 'Campo vazio' : $mensagemErro, 997);
-        if(is_null($valor)) throw new Exception($mensagemErro == '' ? 'Campo não pode ser nulo.' : $mensagemErro, 997);
-        return ['status' => true]; 
-    } else if ($tipo == "time") {
-        if(empty(trim($valor))) throw new Exception($mensagemErro == '' ? 'Campo vazio' : $mensagemErro, 996);
-        if(is_null($valor)) throw new Exception($mensagemErro == '' ? 'Campo não pode ser nulo.' : $mensagemErro, 996);
-        return ['status' => true];
+class Util {
+    
+    public static function converterFormEmArray($form) {
+        $formulario = array();
+        var_dump($form);
+        foreach ($form as $key => $value) {
+            if(strpos($value['name'], '[]')) {
+                $formulario[$value['name']][] = $value['value'];
+            } else $formulario[$value['name']] = trim($value['value']);
+        }
+        return $formulario;
     }
 
-}
+    public static function validarCampo($tipo, $valor, $mensagemErro = '') {
 
-function converterStringParaHora($valor, $padrao = 'H:i:s', $padraoSaida = 'H:i:s') {
-    return DateTime::createFromFormat($padrao, $valor)->format($padraoSaida);
+        if($tipo=="integer") {        
+            if(!is_numeric($valor)) throw new Exception($mensagemErro == '' ? "Não é um valor válido." : $mensagemErro, 999);
+            if(empty($valor)) throw new Exception($mensagemErro == '' ? "Informe o código do Sprint." : $mensagemErro, 998);
+            if($valor <= 0) throw new Exception($mensagemErro == '' ? "Sprint inválido." : $mensagemErro, 998);
+            return ['status'=>true];       
+        } else if ($tipo == "string") {
+            if(empty(trim($valor))) throw new Exception($mensagemErro == '' ? 'Campo vazio' : $mensagemErro, 997);
+            if(is_null($valor)) throw new Exception($mensagemErro == '' ? 'Campo não pode ser nulo.' : $mensagemErro, 997);
+            return ['status' => true]; 
+        } else if ($tipo == "time") {
+            if(empty(trim($valor))) throw new Exception($mensagemErro == '' ? 'Campo vazio' : $mensagemErro, 996);
+            if(is_null($valor)) throw new Exception($mensagemErro == '' ? 'Campo não pode ser nulo.' : $mensagemErro, 996);
+            return ['status' => true];
+        }
+
+    }
+
+    public static function converterStringParaHora($valor, $padrao = 'H:i:s', $padraoSaida = 'H:i:s') {
+        return DateTime::createFromFormat($padrao, $valor)->format($padraoSaida);
+    }
 }
